734811bb3154a86fa1a2f29b80d325769c13f96b
Duan52 <2717144577@qq.com>
2019-06-12 14:14:36 +0800

    宿舍管理界面

---
+++ app/src/main/java/com/example/mysql/ui/DormitoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.mysql.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mysql.R;
+
+public class DormitoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dormitory);
+    }
+}
